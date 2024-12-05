2e133642c4433a2c4e0cb5c28a40c83ea3e35ea9
GGEZXDD <glebgenshin@mail.ru>
2024-12-05 17:29:08 +0300

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,49 +1,39 @@
-from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtWidgets import QPushButton
-from PyQt6.QtGui import QPainter, QColor, QPolygon
-import sys
-import random
-from UI import Ui_MainWindow
 
-SCREEN_SIZE = [680, 480]
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class Example(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.flag = False
-        self.setWindowTitle('Супрематизм')
-        self.pushButton.clicked.connect(self.draw)
-        self.coords = []
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(683, 489)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(430, 420, 241, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 683, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-    def draw(self):
-        self.figure = 'circle'
-        self.size = random.randint(10, 100)
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # 'yellow'
-        self.flag = True
-        self.update()
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def paintEvent(self, event):
-        if self.flag:
-            qp = QPainter()
-            qp.begin(self)
-            qp.setPen(QColor(*self.color))
-            qp.setBrush(QColor(*self.color))
-            self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
-            if self.figure == 'circle':
-                qp.drawEllipse(self.x, self.y, self.size, self.size)
-            qp.end()
-
-
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Показать случайный круг"))
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    sys.excepthook = except_hook
-    ex = Example()
-    ex.show()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec())
